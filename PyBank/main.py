98da0ec107e31a475ed285ffcdd8d4ab35b0439c
Greg Thomas <gthom73@hotmail.com>
2023-12-04 18:36:17 -0600

    routine save

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,7 +14,10 @@ nProfitLoss = 0.0
 average_change = 0.0
 res = []
 large = []
+small = []
 
+
+# -----------------------------------------------------------------------------------------
 
 def sum_numbers(string_list):
 	numbers = [int(num) for num in (string_list)]
@@ -24,32 +27,36 @@ def sum_numbers(string_list):
 def sum_change(string_list):
 	numbers = [int(num) for num in (string_list)]
 	for i in range(1, len(numbers)):
-		res.append(numbers[i] - numbers[i-1])
-	return int(sum(res))
+		res.append(int(numbers[i] - numbers[i-1]))
+		ressum = int(sum(res))
+	return res
 
-def find_largest(string_list):
+def find_largest(dates_list, string_list):
 	numbers = [int(num) for num in (string_list)]
 	for i in range(1, len(numbers)):
 		large.append(numbers[i])
-		max = large[(0)]
-	print(int(large))
-	
 
-	# for i in large:
-	# 	if max > int(large):
-	# 		max = large
-	# return int(max)
+	max = large[0]
+	for i in large:               
+		if i > max:
+			max = i
+			maxDate = dates_list[i - 1]
+	return (max)
+
+def find_smallest(dates_list, string_list):
+	numbers = [int(num) for num in (string_list)]
+	for i in range(1, len(numbers)):
+		small.append(numbers[i])
+
+	min = small[0]
+	for i in small:               
+		if i < min:
+			min = i
+			minDate = dates[i]
+	return minDate, min
 
 
-# def find_smallest(string_list):
-# 	numbers = [int(num) for num in (string_list)]
-# 	for i in range(1, len(numbers)):
-# 		min = numbers[int(0)]
-# 		if max < numbers:
-# 			min = numbers
-# 	return (min)
-
-
+# -----------------------------------------------------------------------------------------
 
 
 with open(profitLoss_csv) as csvfile:
@@ -60,7 +67,7 @@ with open(profitLoss_csv) as csvfile:
 	# print(f"Header: {csvHeader}")
 
 
-
+# -----------------------------------------------------------------------------------------
 
 # # Total Months: 86
 	for row in csvReader:
@@ -81,21 +88,17 @@ print(f"Total: " + "$ " + str((nProfitLoss)))
 # Average Change: $-8311.11
 average_change = sum_change(profitLoss)
 monthsDiviser = tMonths - 1
-
-print(f"Average Change: " + "$ " + str(round(average_change / monthsDiviser, 2)))
+print(f"Average Change: " + "$ " + str(round(int(sum(average_change)) / monthsDiviser, 2)))
 
 
 # Greatest Increase in Profits: Aug-16 ($1862002)
-
-# largestNumber = find_largest(profitLoss)
-# print(f"Greatest Increase in Profits: " + "$ " + str(largestNumber))
+dateOf, largestNumber = find_largest(dates, res)
+print(f"str(largestNumber)"+ str(dateOf)+ " " + str(largestNumber))
 
 
 # Greatest Decrease in Profits: Feb-14 ($-1825558)
-
-# smallestNumber = find_smallest(profitLoss)
-# print(f"Greatest Decrease in Profits: " + "$ " + str(smallestNumber))
-
+smallest_Number = find_smallest(res)
+print(f"str(smallestNumber)" + str(smallest_Number))
 
 
 output_file = os.path.join("budgetResult.csv")
