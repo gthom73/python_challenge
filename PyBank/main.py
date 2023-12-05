c273e9240c9b3b1deebc10ae67c9e296f85b3fc6
Greg Thomas <gthom73@hotmail.com>
2023-12-05 02:01:44 -0600

    Module 3 Challenge, PyBank submission - 12-05-23: 0201 f

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,8 @@
 
 import os
 import csv
-import re
-import statistics
+# import re
+# import statistics
 
 # collect data 
 profitLoss_csv = os.path.join('Resources', 'budget_data.csv')
@@ -30,15 +30,14 @@ with open(profitLoss_csv) as csvfile:
 
 with open(profitLoss_csv, mode='r') as csvfileII:
 	csvReaderII = csv.reader(csvfileII)
+	csvHeaderII = next(csvfileII)
+
 	with open('profitLoss_csv', mode='w') as plOutFile:
 		writer = csv.writer(plOutFile)
 		profitLossDict = {rows[0]:rows[1] for rows in csvReaderII}
 
-print(profitLossDict)
 
-
-
-
+# print(profitLossDict)
 
 # -----------------------------------------------------------------------------------------
 
@@ -56,20 +55,19 @@ def sum_change(string_list):
 
 # -----------------------------------------------------------------------------------------
 
-def find_largest(dates_list, string_list):
+
+def find_largest(string_list):
 	numbers = [int(num) for num in (string_list)]
 	for i in range(1, len(numbers)):
 		large.append(numbers[i])
 
 	max = large[0]
-	maxDate = dates_list[0]
 	for i in large:               
 		if i > max:
 			max = i
-			print(i)
 	return max
 
-def find_smallest(dates_list, string_list):
+def find_smallest(string_list):
 	numbers = [int(num) for num in (string_list)]
 	for i in range(1, len(numbers)):
 		small.append(numbers[i])
@@ -78,46 +76,95 @@ def find_smallest(dates_list, string_list):
 	for i in small:               
 		if i < min:
 			min = i
-			minDate = dates[i]
-	return minDate, min
-
-
-
-
-
-
-
-
+	return min
 
 # -----------------------------------------------------------------------------------------
+print(f"")
+print(f"")
+print(f"")
+print(f"Financial Analysis")
+print(f"")
+print(f'-------------------------------------------------------------')
+print(f"")
+
 
 # # Total Months: 86
 print(f"Total Months: " + str(tMonths))
 
-
+print(f"")
 # Total: $22564198
 nProfitLoss = sum_numbers(profitLoss)
 
-print(f"Total: " + "$ " + str((nProfitLoss)))
+print(f"Total: " + "$ " + str(nProfitLoss))
 
-
+print(f"")
 # Average Change: $-8311.11
 average_change = sum_change(profitLoss)
 monthsDiviser = tMonths - 1
-print(f"Average Change: " + "$ " + str(round(int(sum(average_change)) / monthsDiviser, 2)))
+print(f"Average Change: " + "  $ " + str(round(int(sum(average_change)) / monthsDiviser, 2)))
+print(f"")
 
+# # Greatest Increase in Profits: Aug-16 ($1862002)
+largestNumber = find_largest(res)
+print(f"Greatest Increase in Profits:" + "  $ " + str(largestNumber))
+print(f"")
 
-# Greatest Increase in Profits: Aug-16 ($1862002)
-dateOf, largestNumber = find_largest(dates, res)
-print(f"str(largestNumber)"+ str(dateOf)+ " " + str(largestNumber))
-
-
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
-dateOf,  = find_smallest(dates, res)
-print(f"str(smallestNumber)" + str(smallest_Number))
-
+# # Greatest Decrease in Profits: Feb-14 ($-1825558)
+smallestNumber = find_smallest(res)
+print(f"Greatest Decrease in Profits:" + "  $ " + str(smallestNumber))
+print(f"")
+print(f"")
+print(f"")
 
 output_file = os.path.join("budgetResult.csv")
+
+with open('budgetResult.csv', 'w', newline='') as file:
+	writer = csv.writer(file)
+
+	writer.writerow(["Financial Analysis"])
+	writer.writerow([])
+	writer.writerow(["------------------------------------------------------"])
+	writer.writerow([])
+	writer.writerow(["Total Months: " + str(tMonths)])
+	writer.writerow([])
+	writer.writerow(["Total: " + "  $ " + str(nProfitLoss)])
+	writer.writerow([])
+	writer.writerow(["Average Change: " + "  $ " + str(round(int(sum(average_change)) / monthsDiviser, 2))])
+	writer.writerow([])
+	writer.writerow(["Greatest Increase in Profits:" + "  $ " + str(largestNumber)])
+	writer.writerow([])
+	writer.writerow(["Greatest Decrease in Profits:" + "  $ " + str(smallestNumber)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
